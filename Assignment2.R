997a51d3f214a595e388ecf0d34ba311ab9617b6
rsass1996 <rachel.sass@gmail.com>
2020-06-26 17:56:12 -0400

    Complete assignment with 2 functions and comments

--- Assignment2.R
+++ Assignment2.R
@@ -1,3 +1,9 @@
+# makeCacheMatrix does the following:
+# sets the matrix
+# gets the matrix
+# sets the inverse value
+# gets the inverse value
+
 makeCacheMatrix <- function(x = matrix()) {
      i <- NULL
      set <- function(y) {
@@ -9,6 +15,12 @@ makeCacheMatrix <- function(x = matrix()) {
      getInve <- function() {i}
      list(set = set, get = get,setInv = setInv,getInv = getInv)
   }
+
+# cacheSolve does the following
+# checks to see if inverse value is already cached
+# if the inverse value is cached, it returns that value
+# otherwise, it calculates and returns the inverse value
+
 cacheSolve <- function(x, ...) {
      i <- x$getInv()
      if(!is.null(i)) {
